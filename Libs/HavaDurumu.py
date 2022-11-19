26cb647808b2a5e6199174ef5f45515cb97cb369
keyiflerolsun <keyiflerolsun@gmail.com>
2022-11-19 09:57:14 +0300

    🕊
    🕊

---
+++ Libs/HavaDurumu.py
@@ -0,0 +1,37 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+from CLI     import hata_salla
+from aiohttp import ClientSession
+from asyncio import run
+from atexit  import register as kapanirken
+
+class HavaDurumu:
+    def __oturum_kapa(self):
+        run(self.oturum.close())
+
+    def __init__(self, sehir:str):
+        self.oturum = ClientSession()
+        kapanirken(self.__oturum_kapa)
+
+        self.__api_key = "39bc7d31306a4bf5b5ee4ea4828b6c30"
+        self.sehir     = sehir
+
+    async def ver(self):
+        async with self.oturum.get(
+            url     = "https://api.weatherbit.io/v2.0/current/weather",
+            params  = {
+                "city" : self.sehir,
+                "key"  : self.__api_key
+            },
+        ) as yanit:
+            return await yanit.json()
+
+async def hava_durumu(sehir:str):
+    _hava = HavaDurumu(sehir)
+    sorgu = await _hava.ver()
+    try:
+        veri  = sorgu["data"][0]
+        return f"{veri['ob_time']} » [{veri['country_code']}] {veri['city_name']} - {veri['weather']['description']}"
+    except (KeyError, TypeError):
+        hata_salla(sorgu)
+        return sorgu
